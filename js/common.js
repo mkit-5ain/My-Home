58efc7ed3998d8ff9df7549c21c9e8678d6e5360
임준수-PC\junesu1104 <mkit5ain@gmail.com>
2021-01-07 17:23:37 +0900

    [추가] index

---
+++ js/common.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  fabAction();
+  layoutTab();
+  contentsFade();
+  themeAction();
+  scrollContents();
+  fadeInUp();
+});
+
+function fabAction () {
+  $('.menu-btn').click(function (){
+    $('.menu-view, .menu-btn, body').toggleClass('active');
+  });
+}
+
+function themeAction () {
+  $('.menu-theme').click(function (){
+    $('body, .theme-list').toggleClass('theme-dark');
+  });
+}
+function layoutTab () {
+  $(".category ul li").click(function(){
+      var activeTab = $(this).attr('data-tab');
+      $(".category ul li").removeClass('active');
+      $(".category-contents").removeClass('active');
+      $(this).addClass('active');
+      $("#" + activeTab).addClass('active');
+  });
+}
+
+function contentsFade () {
+  $(window).scroll(function () {
+    $('.fade-contents').each( function () {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if ( bottom_of_window > bottom_of_object ){
+            $(this).addClass('active');
+        }
+    });
+  });
+}
+
+function scrollContents () {
+    $(window).scroll (function () {
+        var Value = ($(window).scrollTop() / 25 );
+        $('.border-circle-wrap').css(
+          { 'transform' : 'translateY(' + Value + 'px)' }
+        );
+    });
+}
+
+function fadeInUp () {
+  $(window).scroll(function () {
+    $('.fadeinup').each( function () {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if ( bottom_of_window > bottom_of_object ){
+            $(this).addClass('fadeinup-active');
+        }
+    });
+  });
+}
+setTimeout(function () {
+  $('.background-circle-wrap').addClass('move');
+  $('.work-list').addClass('move');
+  $('.category-list').addClass('active');
+}, 500);
